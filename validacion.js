c46e4453ec66c1b47e8be27ec0f06e426fc7b06f
IvanCoronel <imc8046@gmail.com>
2021-09-17 12:56:30 -0300

    modificacion de orden de alertas

--- validacion.js
+++ validacion.js
@@ -1,23 +1,34 @@
 function validar() {
-    var error=false;
-    var mensajesError="";
+    var error = false;
+    var mensajesError = "";
 
     var nombre = document.getElementById("nombre").value;
     var asunto  =document.getElementById("asunto").value;
     var mensaje = document.getElementById("mensaje").value;
+    var email = document.getElementById("email").value;
 
-    if(nombre==''){
-        error=true;
+    if(nombre == ''){
+        error = true;
         mensajesError+="<p>Debe escribir su nombre.</p>";
     }
 
-    if(asunto==''){
-        error=true;
+    if(email.length==0){
+        error = true;
+        mensajesError+="<p>El campo e-mail es obligatorio</p>";
+    }else{
+        if(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/.test(email)){
+            error = true;
+            mensajesError+="<p>Por favor ingrese en el campo de email con formato tuemail@gmail.com</p>";
+        }  
+    }
+
+    if(asunto == ''){
+        error = true;
         mensajesError+="<p>El asunto esta incompleto.</p>";
     }
 
-    if(mensaje==''){
-        error=true;
+    if(mensaje == ''){
+        error = true;
         mensajesError+="<p>Debe explicar el motivo de su contacto.</p>";
     }
 
