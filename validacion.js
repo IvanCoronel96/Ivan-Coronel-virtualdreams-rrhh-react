a89fd8e4c9eca37dd391118ea870b429a833226a
IvanCoronel <imc8046@gmail.com>
2021-09-18 17:03:00 -0300

    Modificacion final

--- validacion.js
+++ validacion.js
@@ -1,39 +1,52 @@
 function validar() {
     var error = false;
-    var mensajesError = "";
+    var mensajeNombre = "";
+    var mensajeEmail = "";
+    var mensajeAsunto = "";
+    var mensajeMensaje = "";
 
     var nombre = document.getElementById("nombre").value;
     var asunto  =document.getElementById("asunto").value;
     var mensaje = document.getElementById("mensaje").value;
-    var email = document.getElementById("email").value;
+    var email = document.getElementById("correo").value;
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+    let regexNombre = /^[a-z ,.'-]+$/i;
 
-    if(nombre == ''){
+    if(nombre.length===0){
         error = true;
-        mensajesError+="<p>Debe escribir su nombre.</p>";
+        mensajeNombre+="<p>Debe escribir su nombre.</p>";
+    }else{
+        if( !(regexNombre.test(nombre)) ){
+            error = true;
+            mensajeNombre+="<p>Escriba solo letras.</p>";
+        }  
     }
 
-    if(email.length==0){
+    if(email.length===0){
         error = true;
-        mensajesError+="<p>El campo e-mail es obligatorio</p>";
+        mensajeEmail+="<p>El campo e-mail es obligatorio.</p>";
     }else{
-        if(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/.test(email)){
+        if( !(regexEmail.test(email)) ){
             error = true;
-            mensajesError+="<p>Por favor ingrese en el campo de email con formato tuemail@gmail.com</p>";
+            mensajeEmail+="<p>Su email debe tener formato tuemail@gmail.com</p>";
         }  
     }
 
     if(asunto == ''){
         error = true;
-        mensajesError+="<p>El asunto esta incompleto.</p>";
+        mensajeAsunto+="<p>El asunto esta incompleto.</p>";
     }
 
     if(mensaje == ''){
         error = true;
-        mensajesError+="<p>Debe explicar el motivo de su contacto.</p>";
+        mensajeMensaje+="<p>Debe explicar el motivo de su contacto.</p>";
     }
 
     if(error){
-        document.getElementById("error-datos").innerHTML=mensajesError;
+        document.getElementById("error-nombre").innerHTML=mensajeNombre;
+        document.getElementById("error-email").innerHTML=mensajeEmail;
+        document.getElementById("error-asunto").innerHTML=mensajeAsunto;
+        document.getElementById("error-mensaje").innerHTML=mensajeMensaje;
         return false;
     }else{
         return true;
